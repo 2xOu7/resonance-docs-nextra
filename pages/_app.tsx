6c9a787b90b2748a7bf78c21e7b7d5eac8f057a1
Jonathan Ou <jonathanou@Jonathans-MacBook-Pro.local>
2024-11-06 19:10:24 -0800

    theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
 import "@radix-ui/themes/styles.css";
+import {Theme} from "@radix-ui/themes";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
+export default function MyApp({Component, pageProps}: AppProps) {
+    return <Theme>
+        <Component {...pageProps} />
+    </Theme>
 }
