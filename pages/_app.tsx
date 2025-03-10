34804140bb59463f61424f42f9397a208be74ec9
Jonathan Ou <jonathanou@Jonathans-MacBook-Pro.local>
2025-03-09 21:43:15 -0700

    propel auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import type {AppProps} from 'next/app'
 import "@radix-ui/themes/styles.css";
 import {Theme} from "@radix-ui/themes";
+import {AuthProvider, WithLoggedInAuthInfoProps, withRequiredAuthInfo} from "@propelauth/react";
 
-export default function MyApp({Component, pageProps}: AppProps) {
+interface EnrichedAppProps extends WithLoggedInAuthInfoProps, AppProps {
+
+}
+ function MyApp({Component, pageProps}: EnrichedAppProps) {
     return <Theme>
-        <Component {...pageProps} />
+        <AuthProvider authUrl={process.env.NEXT_PUBLIC_PROPEL_AUTH_URL as string}>
+            <Component {...pageProps} />
+        </AuthProvider>
     </Theme>
 }
+
+export default MyApp
